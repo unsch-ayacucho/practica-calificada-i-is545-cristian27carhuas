3ebbe65fd198f20200cc255c91a066c0c0971636
cristian27carhuas <cristian.carhuas.27@unsch.edu.pe>
2019-07-16 21:55:44 -0500

    primer commnit

---
+++ DivisimaShop/src/main/java/pe/edu/unsch/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pe.edu.unsch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping({"/", "/home"})
+	//@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String index() {
+	return "views/public/home/index";
+	
+	}
+
+}
